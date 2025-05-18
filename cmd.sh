57e18e8e3847bc09e3e7cc843f9ffc92b775b473
Aitor Unizar <delvalalluevaaitor01@gmail.com>
2025-05-18 09:34:31 +0000

    auto exec

---
+++ cmd.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Stop on any error
+set -e
+
+# Create and activate conda environment
+echo "Creating conda environment..."
+conda env create -f rtse_environment.yml -n rtse_env
+
+echo "Activating conda environment..."
+# NOTE: 'conda activate' only works in interactive shells, so use 'source'
+source "$(conda info --base)/etc/profile.d/conda.sh"
+conda activate rtse_env
+
+# Install pip packages
+echo "Installing pip packages..."
+pip install -r rtse_requirements.txt
+
+# Run metrics evaluations
+echo "Running latency measurements..."
+python3 rtse_latency.py
+python3 rtse_latency.py ./configs/config-3.json
+python3 rtse_latency.py ./configs/config-4.json
+python3 rtse_latency.py ./configs/config-5.json
+python3 rtse_latency.py ./configs/config-5.json
+
+# Zip the results
+echo "Zipping results..."
+zip -r latency_results.zip time_profiling
+
+# Deactivate the conda environment
+echo "Deactivating conda environment..."
+conda deactivate
+
+# Remove the conda environment
+echo "Removing conda environment..."
+conda env remove -n rtse_env -y
+
+echo "All tasks completed successfully!"
